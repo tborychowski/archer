8cdb28548b0db0d6c02f35402dd445d5e1255f97
Tomasz Borychowski <tomasz.borychowski@sap.com>
2018-07-22 22:44:47 +0100

    formatting

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
 const Msg = require('node-msg');
-const {login, traffic} = require('./lib/archer-client');
+const {login, traffic, formatBytes} = require('./lib/archer-client');
 
 login()
 	.then(traffic)
 	.then(res => {
+		const sum = res.reduce((p, n) => p + n.bytes, 0);
+		const total = formatBytes(sum);
 		const table = res.map(i => [i.name, i.ip, i.size]);
 		table.unshift(['Name', 'IP', 'Size']);
+		table.push(['', '', '---------'], ['', '', total]);
 		Msg.table(table);
 	});
 
