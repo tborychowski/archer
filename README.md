a1d5006a7c2b43b23311821ab8ae2e5dc7eaddf5
Tomasz Borychowski <tomasz.borychowski@sap.com>
2018-07-24 10:32:54 +0100

    quality & colour

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TP-Link Archer stats
 
+Retrieve per-device traffic stats (upload + download) from the TP-Link Archer router.
+
+
+### Set-up
+
+1. Install dependencies
+    ```sh
+    yarn
+    ```
+2. Rename `config-sample.json` to `config.json` and update details. Devicemap is to show the nice names in the output, instead of (often) ugly names (or codes).
+
+
+### Run
+
+```sh
+node .
+```
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
 const Msg = require('node-msg');
+const chalk = require('chalk');
 const {login, traffic, formatBytes} = require('./lib/archer-client');
 
-login()
-	.then(traffic)
-	.then(res => {
-		const sum = res.reduce((p, n) => p + n.bytes, 0);
-		const total = formatBytes(sum);
-		const table = res.map(i => [i.name, i.ip, i.size]);
-		table.unshift(['Name', 'IP', 'Size']);
-		table.push(['', '', '---------'], ['', '', total]);
-		Msg.table(table);
-	});
+async function start () {
+	const loggedIn = await login();
+	if (!loggedIn) return;
+
+	const res = await traffic();
+	const sum = res.reduce((p, n) => p + n.bytes, 0);
+	const total = formatBytes(sum);
+	const table = res.map(i => [chalk.yellow(i.name), chalk.grey(i.ip), i.size]);
+	table.unshift(['Name', 'IP', 'Size']);
+	table.push(['', '', '---------'], ['', '', chalk.cyan(total)]);
+	Msg.table(table);
+}
+
+
+start();
 
