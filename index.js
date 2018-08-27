72acee9a77edef9be36ae83e4a4817d404d5776f
Tomasz Borychowski <tomasz.borychowski@sap.com>
2018-08-27 16:08:44 +0100

    .

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 const Msg = require('node-msg');
 const chalk = require('chalk');
-const {login, traffic, formatBytes} = require('./lib/archer-client');
+const {login, network, formatBytes} = require('./lib/archer-client');
+
+async function stats () {
+	const loggedIn = await login();
+	if (!loggedIn) return;
+	const res = await network();
+	const sum = res.reduce((p, n) => p + n.bytes, 0);
+	const total = formatBytes(sum);
+	const table = res.map(i => [chalk.yellow(i.name), i.ip, i.size]);
+	table.unshift(['Name', 'IP', 'Size']);
+	table.push(['', '', '---------'], ['', '', chalk.cyan(total)]);
+	Msg.table(table);
+}
 
 async function start () {
 	const loggedIn = await login();
 	if (!loggedIn) return;
-
-	const res = await traffic();
-	const sum = res.reduce((p, n) => p + n.bytes, 0);
-	const total = formatBytes(sum);
-	const table = res.map(i => [chalk.yellow(i.name), chalk.grey(i.ip), i.size]);
-	table.unshift(['Name', 'IP', 'Size']);
-	table.push(['', '', '---------'], ['', '', chalk.cyan(total)]);
+	const res = await network();
+	res.sort((a, b) => a.name.localeCompare(b.name));
+	const table = res.map(i => [chalk.yellow(i.name), i.ip, chalk.grey(i.mac)]);
+	table.unshift(['Name', 'IP', 'Mac']);
 	Msg.table(table);
 }
 
