fc2283fd93e86851b6bfeb38733a1bfd23081a7d
Tomasz Borychowski <tomasz.borychowski@sap.com>
2019-01-20 00:09:31 +0000

    cln

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const Msg = require('node-msg');
 const chalk = require('chalk');
-const {login, all} = require('./lib/archer-client');
+const {login, traffic} = require('./lib/archer-client');
 
 const sortByName =  (a, b) => (''  + a.name).localeCompare('' + b.name);
 const sortByIp =  (a, b) => (+a.ip.split('.').pop()) - (+b.ip.split('.').pop());
@@ -17,15 +17,15 @@ function saveToFile (data) {
 
 function writeToConsole (data) {
 	data.sort(sortByName);
-	const table = data.map(i => [chalk.yellow(i.name), i.ip, chalk.grey(i.mac), i.time, i.size]);
-	table.unshift(['Name', 'IP', 'Mac', 'Time', 'Traffic']);
+	const table = data.map(i => [chalk.yellow(i.name), i.ip, chalk.grey(i.mac), i.size]);
+	table.unshift(['Name', 'IP', 'Mac', 'Traffic']);
 	Msg.table(table);
 }
 
 async function start () {
 	const loggedIn = await login();
 	if (!loggedIn) return;
-	const res = await all();
+	const res = await traffic();
 	saveToFile(res);
 	writeToConsole(res);
 }
